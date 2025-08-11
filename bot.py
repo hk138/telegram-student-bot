5a8dacb84a6cd977b3f55b35bd9cac0ae1c26124
Hadi <152048815+hk138@users.noreply.github.com>
2025-08-11 09:17:26 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 ADMIN_FORUM_ID = os.getenv("ADMIN_FORUM_ID")
 TOKEN = os.getenv("BOT_TOKEN")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+WEBHOOK_URL =  os.getenv("WEBHOOK_URL")
 # پرسش‌ها
 questions = [
     "۱. پایه تحصیلی‌ت چیه؟ (دهم / یازدهم / دوازدهم)",
